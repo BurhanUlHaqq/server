8dc67e4db4445d9cedaeb9070cd179c7e2e591ff
Burhan.Zahir <bhzahir@gmail.com>
2022-09-25 15:39:38 +0500

    new app

--- src/main/java/com/server/server/service/implementation/ServerServiceImpl.java
+++ src/main/java/com/server/server/service/implementation/ServerServiceImpl.java
@@ -6,7 +6,6 @@ import com.server.server.repo.ServerRepo;
 import com.server.server.service.ServerService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.data.domain.Example;
 import org.springframework.data.domain.PageRequest;
 import org.springframework.stereotype.Service;
 
